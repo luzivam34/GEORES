cd7a79eb91bfb66263dc07884bb000faaf2596d6
Luzivam Santos <luzivam.santos28@outlook.com>
2026-02-14 22:31:24 -0300

    commit da modificação do banco de dados organizando codigos

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,21 @@
 from flask import Flask
-
+from config import Config
+from app.extensions import db, migrate
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from .routes.estoque_route import estoque_bp
     from .routes.produto_route import produto_bp
 
     app.register_blueprint(estoque_bp)
     app.register_blueprint(produto_bp)
+
+    with app.app_context():
+        db.create_all()
 
     return app
--- backend/app/models/produto_model.py
+++ backend/app/models/produto_model.py
@@ -3,7 +3,7 @@ from app.extensions import db
 class Produto(db.Model):
     __tablename__ = "produtos"
 
-    id = db.Column(db.Integer, primary_Key=True)
+    id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(150), nullable=False)
     descricao = db.Column(db.Text)
     preco = db.Column(db.Numeric(10,2), nullable=False)
